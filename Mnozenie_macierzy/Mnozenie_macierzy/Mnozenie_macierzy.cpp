3faa099f9ffcac83220755bb338778f0baaebbdf
Michał Cieśla <zwariowanymichal@outlook.com>
2021-10-28 21:55:21 +0200

    Create and sending my resolve

---
+++ Mnozenie_macierzy/Mnozenie_macierzy/Mnozenie_macierzy.cpp
@@ -0,0 +1,95 @@
+﻿#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <cstdlib>
+#include <cstring>
+#include <string>
+#include <iomanip>
+
+using namespace std;
+
+string* S[];
+
+class matrix
+{
+public:
+	long** macierz;
+	matrix()
+	{
+		macierz = new long* [3];
+		for (int i = 0; i < 3; i++)
+		{
+			macierz[i] = new long[3];
+		}
+	}
+	~matrix()
+	{
+		for (int i = 0; i < 3; i++)
+		{
+			delete[] macierz[i];
+		}
+		delete[] macierz;
+	}
+	/*matrix operator*(const matrix &v)
+	{
+		return (this->v * v.macierz[1][1])
+	}
+	matrix operator+(const matrix &v)
+	{
+		return (this->v + v.macierz[1][1])
+	}*/
+};
+
+void read_from_file(int C, int M)
+{
+	string linia;
+	fstream plik;
+	string slowo;
+
+	bool isCandM = true;
+
+	plik.open("cipher*.in", ios::in);
+	if (plik.good() == true)
+	{
+		while (!plik.eof())
+		{
+			getline(plik, linia);
+			if (isCandM)
+			{
+				for (int i = 0; i < linia.size(); i++)
+				{
+					if (linia[i] == ' ')
+					{
+						C = stoi(slowo);
+						slowo.clear();
+					}
+					else if (linia[i] == '\0')
+					{
+						M = stoi(slowo);
+						slowo.clear();
+					}
+					else
+					{
+						slowo[i] = linia[i];
+					}
+				}
+				isCandM = false;
+			}
+			else
+			{
+
+			}
+		}
+		plik.close();
+	}
+}
+
+
+
+int main()
+{
+	matrix A;
+	int C, M;
+	//ladowanie();
+}
